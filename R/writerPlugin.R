b2a17f4b2e3524564210ccc0b7a6d3ce40401836
Paul Young <paul.young@victorchang.edu.au>
2023-09-11 11:11:16 +1000

    Update writerPlugin.R
    modified writerPlugin.R for compatibility with default sc1 dataset.

--- R/writerPlugin.R
+++ R/writerPlugin.R
@@ -7,26 +7,38 @@
 #' @export writerPlugin
 #'
 
-
 writerPlugin <- function(plugInFile,shiny.prefixSet){
 	sierra <- grep("sierra",shiny.prefixSet,value=TRUE,ignore.case=TRUE)
 	expression <- grep("expression",shiny.prefixSet,value=TRUE,ignore.case=TRUE)
 	scRNA <- grep("scRNA",shiny.prefixSet,value=TRUE,ignore.case=TRUE)
 	atac <- grep("atac",shiny.prefixSet,value=TRUE,ignore.case=TRUE)
 	
-	
-	
+	if("sc1" %in% shiny.prefixSet){
+		scRNA <- "sc1"
+	}
+
 	code <- readLines(plugInFile,warn = F)
 	code <- paste0(code,collapse="\n")
-	
-	code <- gsub("\\{sierra\\}",sierra,code,ignore.case=TRUE)
-	code <- gsub("\\{expression\\}",expression,code,ignore.case=TRUE)
-	code <- gsub("\\{scRNA\\}",scRNA,code,ignore.case=TRUE)
-	code <- gsub("\\{atac\\}",atac,code,ignore.case=TRUE)
-	
+
+	c <- try(gsub("\\{sierra\\}",sierra,code,ignore.case=TRUE))
+	if(!identical(class(c),"try-error")){
+		code <- c
+	}
+	c <- try(gsub("\\{expression\\}",expression,code,ignore.case=TRUE))
+	if(!identical(class(c),"try-error")){
+		code <- c
+	}
+	c <- try(gsub("\\{scRNA\\}",scRNA,code,ignore.case=TRUE))
+	if(!identical(class(c),"try-error")){
+		code <- c
+	}
+	c <- try(gsub("\\{atac\\}",atac,code,ignore.case=TRUE))
+	if(!identical(class(c),"try-error")){
+		code <- c
+	}
+
 	code <- paste0(code,"\n")
 	#code <- glue::glue(code)
-	
+
 	return (code)
 }
-
